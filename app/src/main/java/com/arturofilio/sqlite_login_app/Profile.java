29a857dfc50288c46ccf1862ba617ec921735198
Arturo Filio Villa <arturfil@hotmail.com>
2018-08-12 18:25:35 -0400

    added some layout to the profile

--- app/src/main/java/com/arturofilio/sqlite_login_app/Profile.java
+++ app/src/main/java/com/arturofilio/sqlite_login_app/Profile.java
@@ -3,22 +3,30 @@ package com.arturofilio.sqlite_login_app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.arturofilio.sqlite_login_app.models.Account;
+
+import java.text.DecimalFormat;
+import java.util.Calendar;
 
 public class Profile extends AppCompatActivity {
 
     private static final String TAG = "Profile";
 
-    private TextView mTextBgt;
+    private TextView mTextBgt, mDayExp, mTextFullName;
+    private ListView mListViewPmts;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        mTextBgt = (TextView) findViewById(R.id.textBgt);
+        mTextFullName = (TextView) findViewById(R.id.txtName);
+        mTextBgt = (TextView) findViewById(R.id.txtBgt);
+        mDayExp = (TextView) findViewById(R.id.textDayExp);
 
         Intent intent = getIntent();
 
@@ -26,6 +34,19 @@ public class Profile extends AppCompatActivity {
 
         double budget = account.getBudget();
 
-        mTextBgt.setText(String.valueOf(budget));
+        Calendar cal = Calendar.getInstance();
+        double dayS = cal.get(Calendar.DAY_OF_MONTH);
+        double dayEnd = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        double remaining = dayEnd - dayS;
+
+        DecimalFormat df = new DecimalFormat("0.0#");
+
+        String dayExp = df.format(budget / remaining);
+
+        mTextFullName.setText(account.getFullName());
+        mTextBgt.setText(String.valueOf("$" + budget));
+        mDayExp.setText(String.valueOf("$" + dayExp));
+
     }
+
 }
